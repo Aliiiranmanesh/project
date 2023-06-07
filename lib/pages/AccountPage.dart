7e9a3cfff1bb94c3995ba61619e7dc6c131015f7
Ali <ali138210@yahoo.com>
2023-06-07 10:37:32 +0330

    alert added

--- lib/pages/AccountPage.dart
+++ lib/pages/AccountPage.dart
@@ -2,28 +2,80 @@ import 'package:flutter/material.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 import 'package:project/components/IconWidget.dart';
 import 'package:project/pages/accountDetail.dart';
+import 'package:project/pages/PaymentPage.dart';
+import 'package:quickalert/quickalert.dart';
 
-class AccountPage extends StatelessWidget {
+class AccountPage extends StatefulWidget {
   static const keyPassword = "key-password";
 
   const AccountPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => SimpleSettingsTile(
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+
+
+  void showAlert() {
+    var message = '';
+    QuickAlert.show(
+      context: context,
+      type: QuickAlertType.custom,
+      barrierDismissible: true,
+      confirmBtnText: 'پرداخت',
+      customAsset: 'lib/Asset/money.jpg',
+      widget: TextFormField(
+        decoration: const  InputDecoration(
+          alignLabelWithHint: true,
+          hintText: 'مبلغ مورد نظر را پرداخت کنید',
+          prefixIcon: Icon(
+            Icons.attach_money_rounded,
+          ),
+        ),
+        textInputAction: TextInputAction.next,
+        keyboardType: TextInputType.phone,
+        onChanged: (value) => message = value,
+      ),
+      onConfirmBtnTap: () async {
+        if (message.length < 4) {
+          await QuickAlert.show(
+            context: context,
+            type: QuickAlertType.error,
+            text: 'مبلغ کمتر از یک هزار تومان نمیتواند باشد',
+          );
+          return;
+        }
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (context) {
+            return const PaymentPage();
+          },
+        ));
+      },
+  title: 'Custom',
+  text: 'Custom Widget Alert',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) =>
+      SimpleSettingsTile(
         title: 'تنظیمات کاربر',
-        subtitle: 'حریم شخصی و امنیت',
-        leading: IconWidget(icon: Icons.person, color: Colors.green),
+        subtitle: 'حریم شخصی و ارتقا و اقضایش اعتبار',
+        leading: IconWidget(icon: Icons.person, color: Colors.blueGrey),
         child: SettingsScreen(
           title: 'تنظیمات کاربری',
           children: <Widget>[
             buildPrivacy(context),
             buildAccountInfo(context),
+            buildpayment(context),
             buildPassword(),
           ],
         ),
       );
 
-  buildAccountInfo(BuildContext context) => SimpleSettingsTile(
+  buildAccountInfo(BuildContext context) =>
+      SimpleSettingsTile(
         title: 'اطلاعات کاربر',
         subtitle: '',
         leading: IconWidget(
@@ -39,21 +91,37 @@ class AccountPage extends StatelessWidget {
         },
       );
 
-  Widget buildPrivacy(BuildContext context) => SimpleSettingsTile(
-        title: 'حریم شخصی',
+  Widget buildPrivacy(BuildContext context) =>
+      SimpleSettingsTile(
+        title: 'ارتقا حساب کاربری',
         subtitle: '',
         leading: IconWidget(
-          icon: Icons.lock,
+          icon: Icons.upgrade_rounded,
           color: Colors.blue,
         ),
         onTap: () {},
       );
 
-  Widget buildPassword() => TextInputSettingsTile(
-        settingKey: keyPassword,
+  buildpayment(BuildContext context) =>
+      SimpleSettingsTile(
+        title: 'افزایش اعتبار',
+        subtitle: '',
+        leading: IconWidget(
+          icon: Icons.monetization_on_rounded,
+          color: Colors.teal,
+        ),
+        onTap: () {
+          showAlert();
+        },
+      );
+
+  Widget buildPassword() =>
+      TextInputSettingsTile(
+        settingKey: AccountPage.keyPassword,
         title: 'رمز ورود',
         obscureText: true,
-        validator: (password) => password != null && password.length >= 6
+        validator: (password) =>
+        password != null && password.length >= 6
             ? null
             : '!بیش از شش کارکتر وارد کن',
       );
---
+++ lib/pages/PaymentPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
+
+class PaymentPage extends StatefulWidget {
+  const PaymentPage({Key? key}) : super(key: key);
+
+  @override
+  State<PaymentPage> createState() => _PaymentPageState();
+}
+
+class _PaymentPageState extends State<PaymentPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
